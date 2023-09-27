5cc5a95a0df7d2c3b6b33e20503912e6e6999dcc
BillyBoyo <john@doe.com>
2023-09-27 18:50:36 +0200

    banner front page

--- app/public/wp-content/themes/montheme/front-page.php
+++ app/public/wp-content/themes/montheme/front-page.php
@@ -1,11 +1,22 @@
-<?php get_header(); ?>
+<?php get_header(); 
 
-	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-    
-    	<h1><?php the_title(); ?></h1>
-    
-    	<?php the_content(); ?>
+$args = array(
+    'post_type' => 'photo', 
+    'posts_per_page' => 1,
+    'orderby' => 'rand' // mode aléatoire
+);
 
-	<?php endwhile; endif; ?>
+$random_photo = get_posts($args);
+
+if ($random_photo) {
+
+	$photo_url = get_the_post_thumbnail_url($random_photo[0], 'full'); // Récupère l'URL de l'image
+    echo '<div class="banner">';
+    echo '<img src="' . $photo_url . '" alt="Photo aléatoire">';
+    echo '<H1>PHOTOGRAPHE EVENT</H1>';
+    echo '</div>';
+}
+?>
+
 
 <?php get_footer(); ?>
